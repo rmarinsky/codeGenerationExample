ca6fb349f1a21629f427610b4df1fa7f30c32c88
Roman Marinsky <newromka@gmail.com>
2021-06-06 16:52:22 +0300

    AddNewPetToStore implemented

--- src/test/java/io/swagger/petstore/PetStoreInRawStyleTests.java
+++ src/test/java/io/swagger/petstore/PetStoreInRawStyleTests.java
@@ -2,6 +2,33 @@ package io.swagger.petstore;
 
 public class PetStoreInRawStyleTests {
 
+    private final String
+            baseUrl = "http://petstore.swagger.io/v2",
+            apiKeyValue = "1qa2ws3ed4rfvcxz";
 
+    @Test
+    public void addNewPetToStoreTest() {
+        String testPetId = RandomStringUtils.randomNumeric(10),
+                testPetName = "Pet_" + RandomStringUtils.randomAlphabetic(10);
+
+        //Add new pet to the store
+        RestAssured.given().baseUri(baseUrl)
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"id\": " + testPetId + ",\n" +
+                        "  \"name\": \"" + testPetName + "\",\n" +
+                        "  \"photoUrls\": [],\n" +
+                        "  \"tags\": [],\n" +
+                        "  \"status\": \"pending\"\n" +
+                        "}")
+                .header("api_key", apiKeyValue)
+                .post("/pet")
+
+                .then()
+                .body("name", CoreMatchers.equalTo(testPetName))
+                .and()
+                .body("id", Matchers.hasToString(testPetId));
+    }
 
 }
