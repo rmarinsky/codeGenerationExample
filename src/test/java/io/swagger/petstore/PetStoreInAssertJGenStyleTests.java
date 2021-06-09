063021c8d12f306781af40418dc0e6e9a3afdbf4
Roman Marinsky <newromka@gmail.com>
2021-06-09 15:33:10 +0300

    Implemented test based on assertj generator

---
+++ src/test/java/io/swagger/petstore/PetStoreInAssertJGenStyleTests.java
@@ -0,0 +1,44 @@
+package io.swagger.petstore;
+
+import com.github.javafaker.Faker;
+import io.restassured.response.Response;
+import io.swagger.petstore.asserts.PetAssert;
+import io.swagger.petstore.controller.PetController;
+import io.swagger.petstore.models.Category;
+import io.swagger.petstore.models.Pet;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class PetStoreInAssertJGenStyleTests {
+
+    private final PetController petController = new PetController();
+    private final Faker faker = new Faker();
+
+    @Test
+    @DisplayName("Create and get pet by id")
+    void createAndGetPet() {
+        var targetPet = new Pet()
+                .id(faker.number().randomNumber())
+                .name(faker.name().name())
+                .category(new Category()
+                        .id(faker.number().randomNumber())
+                        .name(faker.name().username())
+                ).status(Pet.StatusEnum.AVAILABLE)
+                .photoUrls(new ArrayList<>())
+                .tags(new ArrayList<>());
+
+        Response pet = petController.createPet(targetPet);
+        Assertions.assertThat(pet.getStatusCode()).isEqualTo(200);
+
+        PetAssert.assertThat(pet.as(Pet.class)).hasId(targetPet.getId());
+
+        var petById = petController.getPetById(targetPet.getId().toString());
+
+        PetAssert.assertThat(petById.as(Pet.class)).isEqualTo(targetPet);
+
+    }
+
+}
